333c9e2ae5c3b45e2f0cc72a130579a42fe7f073
AlexZ <alex.zxm@foxmail.com>
2020-08-20 22:23:15 -0500

    subschema

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -3,3 +3,43 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
+
+connect.then((db) =>{
+    console.log('Connected correctly to server.');
+
+     Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+        .then((dishes) =>{
+            console.log(dishes);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+            return dish.save();
+     })
+        .then(()=>{
+            console.log(dish);
+
+            return Dishes.remove({});
+        })
+        .then(() =>{
+            return mongoose.connection.close();
+        }).catch((err) =>{
+            console.log(err);
+        });
+
+});
